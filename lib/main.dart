55e49866b903482d61d0898507fec443e290b2a5
bangbangmerch <21-11089@g.batstate-u.edu.ph>
2024-06-26 11:44:01 +0800

    yaan na

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:bangbangmerch1/home.dart';
+import 'package:bangbangmerch1/pages/dashboard.dart';
+import 'package:bangbangmerch1/pages/menu.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  var deepPurpleurple;
-  runApp(MaterialApp(
-    home: Home()
-  ),
-  );
-}
+
+void main() => runApp(MaterialApp(
+    routes: {
+      '/' :(context) => Dashboard(),
+      '/menu' : (context) => Menu(),
+      '/profile' : (context) => Home(),
+    },
+  ));
