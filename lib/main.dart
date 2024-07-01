bee01d42190b3ea25818a4374bfd4935430bd7ab
21-11089@g.batstate-u.edu.ph <21-11089@g.batstate-u.edu.ph>
2024-07-01 11:33:31 +0800

    Sign Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:bangbangmerch1/home.dart';
 import 'package:bangbangmerch1/pages/dashboard.dart';
 import 'package:bangbangmerch1/pages/menu.dart';
+import 'package:bangbangmerch1/pages/signup.dart';
 import 'package:flutter/material.dart';
 
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/menu',
+  initialRoute: '/signup',
     routes: {
       '/' :(context) => Dashboard(),
       '/menu' : (context) => Menu(),
       '/profile' : (context) => Home(),
+      '/signup' : (context) => Signup(),
     },
   ));
